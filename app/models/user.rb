a6b3727ddc3582f4a6577fd28e8bdcd99bd376ed
yasuda <tasssra@gmail.com>
2019-03-19 16:36:32 +0900

    deviseのマイグレーションファイル削除

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :lockable, :timeoutable, :omniauthable, omniauth_providers: [:twitter]
-end
