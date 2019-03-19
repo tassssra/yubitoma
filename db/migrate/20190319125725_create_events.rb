a6cabca29942446698a0998ff1e8110e9450efed
yasuda <tasssra@gmail.com>
2019-03-19 23:48:58 +0900

    Eventモデル生成、DBに反映

--- db/migrate/20190319125725_create_events.rb
+++ db/migrate/20190319125725_create_events.rb
@@ -1,10 +1,10 @@
 class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
-      t.string :title
-      t.date :date
-      t.text :description
-      t.string :address
+      t.string :title, null: false, length: 30
+      t.date :date, null: false
+      t.text :description, null:false, length: 1000
+      t.string :address, null:false
 
       t.timestamps
     end
