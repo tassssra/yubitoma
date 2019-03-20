c813ce0a5ae4eca3a9a4460c3536b5c01ba1bb94
yasuda <tasssra@gmail.com>
2019-03-20 11:52:31 +0900

    user_eventモデルの生成、DBに反映

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ApplicationRecord
+  has_many :users, through: :user_events
+  has_many :user_events
+  accepts_nested_attributes_for :user_events
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :events, through: :user_events
+  has_many :user_events
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :timeoutable
