f68809c8a999fa073ec7b8184db8379c4fe80270
yasuda <tasssra@gmail.com>
2019-03-21 18:05:36 +0900

    UserEventモデル、テーブルをJoin, joinsにそれぞれ変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :events, through: :user_events
-  has_many :user_events
+  has_many :events, through: :joins
+  has_many :joins
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_20_023522) do
+ActiveRecord::Schema.define(version: 2019_03_21_081721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,13 +24,13 @@ ActiveRecord::Schema.define(version: 2019_03_20_023522) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "user_events", force: :cascade do |t|
+  create_table "joins", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["event_id"], name: "index_user_events_on_event_id"
-    t.index ["user_id"], name: "index_user_events_on_user_id"
+    t.index ["event_id"], name: "index_joins_on_event_id"
+    t.index ["user_id"], name: "index_joins_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -59,6 +59,6 @@ ActiveRecord::Schema.define(version: 2019_03_20_023522) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
-  add_foreign_key "user_events", "events"
-  add_foreign_key "user_events", "users"
+  add_foreign_key "joins", "events"
+  add_foreign_key "joins", "users"
 end
