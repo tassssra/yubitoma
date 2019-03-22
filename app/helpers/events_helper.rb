1d5034e000e1bc5fc18bd2f28ce5bd94d76a3f9f
yasuda <tasssra@gmail.com>
2019-03-22 15:57:59 +0900

    編集画面実装、showビューの一部をヘルパーに移動

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,10 @@
+# app/helpers/events_helper.rb
 module EventsHelper
+  def show_event_image(event)
+    if event.image.attached?
+      image_tag event.image.variant(resize: "300x300"), class: "img-thumbnail"
+    else
+      image_tag "no_image.png", class: "img-thumbnail"
+    end
+  end
 end
