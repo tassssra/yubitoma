26435b06cc7a64ade70871c865ecd2873c9d3b72
yasuda <tasssra@gmail.com>
2019-03-22 12:23:38 +0900

    eventのバリデーション設定、errorsパーシャル作成

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,9 @@ class Event < ApplicationRecord
   has_many :users, through: :joins
   has_many :joins
   accepts_nested_attributes_for :joins
+
+  validates :title, presence: true, length: 1..50
+  validates :date, presence: true, numericality: true
+  validates :address, presence: true
+  validates :description, presence: true, length: { maximum: 1000 }
 end
