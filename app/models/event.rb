aa5040dc3eef110d3499da5fb76754202d1315f5
yasuda <tasssra@gmail.com>
2019-03-22 17:52:39 +0900

    削除機能実装

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,7 +8,7 @@ class Event < ApplicationRecord
   accepts_nested_attributes_for :joins
 
   validates :title, presence: true, length: 1..50
-  validates :date, presence: true, numericality: true
+  validates :date, presence: true
   validates :address, presence: true
   validates :description, presence: true, length: { maximum: 1000 }
 
