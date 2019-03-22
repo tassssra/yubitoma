ead430a98f1ff9e6934a0a28ce0c14a4ff8b79c6
yasuda <tasssra@gmail.com>
2019-03-22 10:02:59 +0900

    eventsビューページ作成中

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   has_many :events, through: :joins
   has_many :joins
 
+  def join(event)
+    joins.find_or_create_by(event_id: event.id)
+  end
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :timeoutable
