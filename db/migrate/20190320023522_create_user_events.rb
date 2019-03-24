ccf33756c472ddb0a6eb33a4f77c03670e0c57cd
yasuda <tasssra@gmail.com>
2019-03-24 15:56:12 +0900

    joinsテーブルにユニーク制約をつけた

--- db/migrate/20190320023522_create_user_events.rb
+++ db/migrate/20190320023522_create_user_events.rb
@@ -1,6 +1,6 @@
 class CreateUserEvents < ActiveRecord::Migration[5.2]
   def change
-    create_table :user_events do |t|
+    create_table :join do |t|
       t.references :user, foreign_key: true
       t.references :event, foreign_key: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_22_060036) do
+ActiveRecord::Schema.define(version: 2019_03_24_064944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2019_03_22_060036) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_joins_on_event_id"
+    t.index ["user_id", "event_id"], name: "index_joins_on_user_id_and_event_id", unique: true
     t.index ["user_id"], name: "index_joins_on_user_id"
   end
 
