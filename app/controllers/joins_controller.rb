e87b970999c6b75dc43a9929a208f6f0041a34e4
yasuda <tasssra@gmail.com>
2019-03-25 15:44:03 +0900

    参加・キャンセルでnotice表示するようにした

--- app/controllers/joins_controller.rb
+++ app/controllers/joins_controller.rb
@@ -2,11 +2,13 @@ class JoinsController < ApplicationController
   def create
     @join = current_user.joins.create(event_id: params[:event_id])
     redirect_back(fallback_location: root_path)
+    flash[:notice] = "イベントに参加しました。"
   end
 
   def destroy
     @join = Join.find_by(event_id: params[:event_id], user_id: current_user.id)
     @join.destroy
       redirect_back(fallback_location: root_path)
+    flash[:notice] = "イベント参加をキャンセルしました。"
   end
 end
