b66122773434cb1700c8041e47d7c6cbb559a07c
yasuda <tasssra@gmail.com>
2019-03-25 16:52:22 +0900

    seedデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Event.create!(
+         id: 1,
+         title: "渋谷LT会",
+         date: "2019-03-29",
+         description: "個人開発サービスの発表会です",
+         address: "渋谷道玄坂",
+         created_at: "2019-04-02 02:04:12",
+         updated_at: "2019-03-25 02:04:12",
+         user_id: 1,
+         new_image: nil
+)
+
+Event.create!(
+         id: 2,
+         title: "Rubyカンファレンス",
+         date: "2019-03-30",
+         description: "世界的なカンファレンス",
+         address: "東京",
+         created_at: "2019-03-25 06:36:45",
+         updated_at: "2019-03-25 06:36:45",
+         user_id: 1,
+         new_image: nil
+)
